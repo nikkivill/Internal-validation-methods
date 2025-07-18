ffa453edc9eb425fbe1917032e6e80c471a0c6a8
nikkivill <149186766+nikkivill@users.noreply.github.com>
2025-07-18 14:49:07 +0100

    Update external_val_functions.py

--- transcriptomic_gene_expression/external_validation/external_val_functions.py
+++ transcriptomic_gene_expression/external_validation/external_val_functions.py
@@ -15,7 +15,7 @@ def external_val(model, threshold,
                  clf_method, fit=False, scaler=None):
     
     """
-    Applies external validation for a model, giving the option to refit on all of the data used for development. 
+    Applies external validation for a model, giving the option to fit on all of the data used for development. 
 
     Parameters:
     - model (sklearn estimator): Optimised machine learning classifier obtained from internal validation 
