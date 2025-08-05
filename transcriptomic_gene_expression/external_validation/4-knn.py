2316e537a8fa3bbe8c1df8428641f7a9ef890837
nikkivill <149186766+nikkivill@users.noreply.github.com>
2025-08-05 18:07:00 +0100

    Add files via upload

---
+++ transcriptomic_gene_expression/external_validation/4-knn.py
@@ -0,0 +1,96 @@
+
+### K-Nearest Neighbour (KNN)
+
+# import necessary libraries
+import joblib
+import pandas as pd
+import numpy as np
+from numpy import argmax
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import roc_auc_score, accuracy_score, confusion_matrix, roc_curve
+
+# import function
+from external_val_functions import external_val, plot_roc_curves
+
+#load objects
+X_train = joblib.load("../input/X.pkl")
+Y_train  = joblib.load("../input/Y.pkl")
+
+X_test= joblib.load("../input/ex_X.pkl")
+Y_test  = joblib.load("../input/ex_Y.pkl")
+
+print("KNN classifiers - external validation\n")
+
+knn_tts_model = joblib.load("../input/knn_tts_model.pkl")
+knn_tts_threshold = joblib.load("../input/knn_tts_threshold.pkl")
+knn_tts_scaler = joblib.load("../input/knn_tts_scaler.pkl")
+
+knn_bs_model = joblib.load("../input/knn_bs_model.pkl") 
+knn_bs_threshold  = joblib.load("../input/knn_bs_threshold.pkl")
+
+knn_kcv_model = joblib.load("../input/knn_kcv_model.pkl") 
+knn_kcv_threshold = joblib.load("../input/knn_kcv_threshold.pkl")
+
+knn_logocv_model = joblib.load("../input/knn_logocv_model.pkl") 
+knn_logocv_threshold = joblib.load("../input/knn_logocv_threshold.pkl")
+
+# save roc data
+roc_data = []
+
+# Train-test split model
+tts_metrics, tts_fpr, tts_tpr, tts_auc = external_val(model = knn_tts_model,
+             threshold = knn_tts_threshold,
+             X_train = X_train,
+             Y_train = Y_train,
+             X_test = X_test,
+             Y_test = Y_test,
+             clf_method = "KNN - Train-test split",
+             fit = False,
+             scaler = knn_tts_scaler)
+
+roc_data.append((tts_fpr, tts_tpr, tts_auc, "Train-test split"))
+
+# Bootstrapping model
+bs_metrics, bs_fpr, bs_tpr, bs_auc = external_val(model = knn_bs_model,
+             threshold = knn_bs_threshold,
+             X_train = X_train,
+             Y_train = Y_train,
+             X_test = X_test,
+             Y_test = Y_test,
+             clf_method = "KNN - Bootstrapping",
+             fit = True)
+
+roc_data.append((bs_fpr, bs_tpr, bs_auc, "Bootstrapping"))
+
+# K-fold CV model
+kcv_metrics, kcv_fpr, kcv_tpr, kcv_auc = external_val(model = knn_kcv_model,
+             threshold = knn_kcv_threshold,
+             X_train = X_train,
+             Y_train = Y_train,
+             X_test = X_test,
+             Y_test = Y_test,
+             clf_method = "KNN - 5-fold CV",
+             fit = True)
+
+roc_data.append((kcv_fpr, kcv_tpr, kcv_auc, "5-fold CV"))
+
+# LOGO-CV model
+logocv_metrics, logocv_fpr, logocv_tpr, logocv_auc = external_val(model = knn_logocv_model,
+             threshold = knn_logocv_threshold,
+             X_train = X_train,
+             Y_train = Y_train,
+             X_test = X_test,
+             Y_test = Y_test,
+             clf_method = "KNN - LOGOCV",
+             fit = True)
+
+roc_data.append((logocv_fpr, logocv_tpr, logocv_auc, "LOGOCV"))
+
+
+# Plot all ROC curves together
+plot_roc_curves(
+    roc_data=roc_data,
+    plot_title="KNN - External Validation ROC Curves",
+    save_path="results/knn_all_val_roc_curve.png"
+)
---
+++ transcriptomic_gene_expression/external_validation/j4-knn.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -l h_rt=240:00:00
+#$ -pe smp 1
+#$ -l h_vmem=5G
+#$ -cwd
+#$ -j y
+#$ -m beas
+ 
+# load module
+module load python
+ 
+# activate virtual environment
+source /data/home/bt19531/myenv/bin/activate
+ 
+# run python script
+python 4-knn.py
+
